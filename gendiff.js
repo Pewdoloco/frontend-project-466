00b46c3717f7df9e04b2efd96b11e77bf5d3e2dc
Lev <wleow.ershov@gmail.com>
2024-11-06 03:16:37 -0600

    add "Arguments and options"

--- gendiff.js
+++ gendiff.js
@@ -6,4 +6,6 @@ const program = new Command();
 program
   .version('12.1.0')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>') 
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
