e3c9a4baeba7b303140ce5ff44aac3f871936b84
Lev <wleow.ershov@gmail.com>
2024-11-08 09:30:57 -0600

    update "gameDiff.js"

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,27 +1,24 @@
+import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
-import _ from 'lodash';
+
+const parseFile = (filepath) => JSON.parse(fs.readFileSync(path.resolve(filepath), 'utf-8'));
+
+const getSortedKeys = (obj1, obj2) => _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
+
+const formatDiff = (key, data1, data2) => {
+    if (data1[key] === data2[key]) return `  ${key}: ${data1[key]}`;
+    if (data1[key] && !data2[key]) return `- ${key}: ${data1[key]}`;
+    if (!data1[key] && data2[key]) return `+ ${key}: ${data2[key]}`;
+    return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
+};
 
 const genDiff = (filepath1, filepath2) => {
-  const file1Data = JSON.parse(fs.readFileSync(path.resolve(filepath1), 'utf-8'));
-  const file2Data = JSON.parse(fs.readFileSync(path.resolve(filepath2), 'utf-8'));
-
-  const allKeys = _.sortBy(_.union(Object.keys(file1Data), Object.keys(file2Data)));
-
-  const result = allKeys.map((key) => {
-    if (_.has(file1Data, key) && !_.has(file2Data, key)) {
-      return `  - ${key}: ${file1Data[key]}`;
-    }
-    if (!_.has(file1Data, key) && _.has(file2Data, key)) {
-      return `  + ${key}: ${file2Data[key]}`;
-    }
-    if (file1Data[key] !== file2Data[key]) {
-      return `  - ${key}: ${file1Data[key]}\n  + ${key}: ${file2Data[key]}`;
-    }
-    return `    ${key}: ${file1Data[key]}`;
-  });
-
-  return `{\n${result.join('\n')}\n}`;
+    const file1Data = parseFile(filepath1);
+    const file2Data = parseFile(filepath2);
+    const allKeys = getSortedKeys(file1Data, file2Data);
+    
+    return allKeys.map((key) => formatDiff(key, file1Data, file2Data)).join('\n');
 };
 
 export default genDiff;
